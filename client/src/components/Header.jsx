bda5ad46e3b70763347242abf1e9324f163900be
Andreas Papakostas <andreaspkst@gmail.com>
2024-02-25 01:18:25 +0200

    Created and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -28,7 +28,7 @@ export default function Header() {
                 <FaMoon />
             </Button>
             <Link>
-                <Button gradientDuoTone='purpleToBlue'>
+                <Button gradientDuoTone='purpleToBlue' outline>
                     SignIn
                 </Button>
             </Link>
